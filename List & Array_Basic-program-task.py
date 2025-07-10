611125232cd3a784ba9a6f67a84903fb66345fa7
meet-irfan <irfanali.bscsf21kdk@iba-suk.edu.pk>
2025-07-10 17:13:21 +0500

    Task-1

---
+++ List & Array_Basic-program-task.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import array
+
+# Using list
+numbers_list = [1, 2, 3, 4]
+
+# Using array
+import array
+numbers_array = array.array('i', [1, 2, 3, 4])  # 'i' for signed int
+
+
+# In[3]:
+
+
+print(numbers_array)
+
+
+# In[4]:
+
+
+import numpy as np
+matrix = np.array([[1, 2], [3, 4]])
+
+
+# In[5]:
+
+
+print(matrix)
+
+
+# In[ ]:
+
+
+
+
